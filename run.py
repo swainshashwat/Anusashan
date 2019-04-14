c32f966c615cae5efd1e761c694d005a51ab4bb8
Shashwat Swain <shashwatswainofficial@gmail.com>
2019-04-15 01:51:08 +0530

    cleaned result files

---
+++ run.py
@@ -0,0 +1,57 @@
+import dlib
+import cv2
+import numpy as np
+from renderFace import renderFace
+
+def writeLandmarksToFile(landmarks, landmarksFileName):
+  with open(landmarksFileName, 'w') as f:
+    for p in landmarks.parts():
+      f.write("%s %s\n" %(int(p.x),int(p.y)))
+
+  f.close()
+
+# Landmark model location
+PREDICTOR_PATH = "landmark_model/shape_predictor_68_face_landmarks.dat"
+
+# Get the face detector
+print('Load Face Detector')
+faceDetector = dlib.get_frontal_face_detector()
+
+# The landmark detector is implemented in the shape_predictor class
+print('Load Facial Landmark Predictor')
+landmarkDetector = dlib.shape_predictor(PREDICTOR_PATH)
+
+# Init Video Capture
+print('Init Video Capture')
+cap = cv2.VideoCapture(0)
+
+while True:
+
+  # reading video
+  ret, img = cap.read()
+
+  # Detect faces in the image
+  faceRects = faceDetector(img, 0)
+  
+  # Loop over all detected face rectangles
+  for i in range(0, len(faceRects)):
+    
+    newRect = dlib.rectangle(int(faceRects[i].left()),int(faceRects[i].top()),
+                            int(faceRects[i].right()),int(faceRects[i].bottom()))
+
+    # For every face rectangle, run landmarkDetector
+    landmarks = landmarkDetector(img, newRect)
+
+
+    # Draw landmarks on face
+    renderFace(img, landmarks)
+
+  # Display Image
+  cv2.imshow("Facial Landmark detector", img)
+
+  if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+      
+cap.release()
+cv2.destroyAllWindows()
